29e7b4a0234daed9b2da979186a4184657453759
Jędrzej Usik <255536@student.pwr.edu.pl>
2022-03-13 22:08:54 +0100

    xD

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "namespace_std.h"
+#include <string.h>
+int main()
+{
+    string name;
+    cout<<"podaj nazwe"<<endl;
+    cin>>name;
+    cout<<"podana nazwa: "<<name<<endl;
+    return 0;
+}
+// Created by usiox on 13.03.2022.
+//
+
---
+++ namespace_std.h
@@ -0,0 +1,11 @@
+//
+// Created by usiox on 13.03.2022.
+//
+#include <iostream>
+#ifndef RANDOMNESS_TEST_NAMESPACE_STD_H
+#define RANDOMNESS_TEST_NAMESPACE_STD_H
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+#endif //RANDOMNESS_TEST_NAMESPACE_STD_H
