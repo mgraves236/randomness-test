f1aff722fb0948ba7ece01db0069e7cde10f61ed
Vegetto61299 <101362535+Vegetto61299@users.noreply.github.com>
2022-03-13 21:26:03 +0100

    Delete Jedrzej.h

--- Jedrzej.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string.h>
-void imie()
-{
-	std::string imie;
-	std::cout << "wprowadz imie" << std::endl;
-	std::cin >> imie;
-	std::cout << "Podane imie to: " << imie << std::endl;
-}
