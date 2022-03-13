e97284e81565d0682e9f93e51282836de21f9ec0
Jędrzej Usik <255536@student.pwr.edu.pl>
2022-03-13 20:22:55 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "potatoe.h"
 
-void main(){
+int main(){
  cout<<"Hello World"<<endl; 
+ int trawa = 1;
+ int zielona = 2;
+ if (trawa == zielona)
+ {
+	 return 0;s
+ }
+ return 0;
 }
