2026d95ce2a7c0796dda1f8f80895390e0bf184c
vegetto61299 <vegetto61299@interia.pl>
2022-03-18 16:23:33 +0100

    https://www.youtube.com/watch?v=IXFjJGZlzEE commit by Vegetto, trolling is my job

--- main.cpp
+++ main.cpp
@@ -11,10 +11,9 @@ int main()
 /*  Why this program is stupid?
  *  We are translating 8 bits into char to write it into a file as 8*8 bits (1 and 0 in txt file are bytes-8 bits)
  *  In this situation we are wasting 8x more disk capacity than needed
- *  Next we are comparing these 1 and 0 as string to other 1 and 0 strings that we made from int, in this situation instead
- *  of comparing fe. 10010 to 11111 as bits we are comparing "10010" to "11111" strings that contain 40 bits each
- *  instead of 5. That's because Vegetto is stupid - he can't write more efficient code, and c++ is stupid - it won't
- *  allow for comparsion of single bits and won't let use single bits as variables nor variable array.
+ *  Next we are comparing these 1 and 0 as string to other 1 and 0 strings that we made from int, in this situation
+ *  instead of comparing fe. 10010 to 11111 as bits we are comparing "10010" to "11111" strings that contain 40 bits
+ *  each instead of 5. That's because Vegetto is stupid - he should use std::bitset
  *
  *  "Brainfuck>C++"
  *  ~Vegetto
